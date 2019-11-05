5febb00f9d231d8e24c18843d6e1a870445e924a
Chris Speck <cgspeck@gmail.com>
2019-11-05 18:30:32 +1100

    Skip homebrew update due to TravisCI bug, also attempt to build Ubuntu 18.04 binaries

---
+++ release-scripts/docker-build-and-release-ubuntu-1804.sh
@@ -0,0 +1,24 @@
+#! /bin/bash -e
+source $DIR/common-vars
+
+BUILD_PATH=/app/build
+TARGETS=("ubuntu1804")
+PACKAGES=("brewtarget_2.4.0_x86_64.deb" "brewtarget_2.4.0_x86_64.rpm" "brewtarget_2.4.0_x86_64.tar.bz2")
+
+if [[ "$TRAVIS" == "true" ]]; then
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+fi
+
+for target in ${TARGETS[*]}; do
+  tag="${target}"
+  set +e
+  docker pull cgspeck/brewtarget-build:$tag
+  set -e
+  docker build -t $tag -f Dockerfile-$target .
+  for package in ${PACKAGES[*]}; do
+    docker run --rm --entrypoint cat $tag $BUILD_PATH/$package > packages/$target_$package
+  done
+  docker push cgspeck/brewtarget-build:$tag
+done
+
+# TODO: upload packages to GitHub!
