cdb61ecfa641ad6ca51f02e2443853a5df61594b
Chris Speck <cgspeck@gmail.com>
2019-11-09 13:28:17 +1100

    wip build scripts for archlinux

---
+++ release-scripts/docker-build-and-release-archlinux.sh
@@ -0,0 +1,47 @@
+#! /bin/bash -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $DIR/common-vars
+
+TARGET="arch"
+BUILD_PATH=/app/arch/
+PACKAGES=("brewtarget_2.4.0_x86_64.deb" "brewtarget_2.4.0_x86_64.rpm" "brewtarget_2.4.0_x86_64.tar.bz2")
+
+if [[ "$TRAVIS" == "true" ]]; then
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+fi
+
+echo "Building for ${TARGET}"
+
+tag="${TARGET}"
+expanded_tag="${tag}-${SHORT_HASH}"
+
+docker build -t cgspeck/brewtarget-build:$tag -f Dockerfile-$TARGET .
+for package in ${PACKAGES[*]}; do
+  docker run --rm --entrypoint cat cgspeck/brewtarget-build:$tag $BUILD_PATH/$package > "packages/${TARGET}_${package}"
+done
+echo -e "\nPackages:"
+ls packages/
+echo -e "\nPushing new docker images"
+docker push cgspeck/brewtarget-build:$tag
+
+echo -e "\nDownloading github-releases tool"
+tmp_dir=$(mktemp -d)
+github_release_archive=$tmp_dir/github-release.tar.bz2
+github_release_path=$tmp_dir/bin/linux/amd64/github-release
+wget "https://github.com/aktau/github-release/releases/download/v0.7.2/linux-amd64-github-release.tar.bz2" -O $github_release_archive
+tar xvjf $github_release_archive -C $tmp_dir
+chmod +x $github_release_path
+echo -e "\nUploading binaries to Github"
+
+
+for package in ${PACKAGES[*]}; do
+  src="./packages/${TARGET}_${package}"
+  echo -e "\nUploading ${src}"
+  $github_release_path upload \
+    --user cgspeck \
+    --repo brewtarget \
+    --tag $TAG_NAME \
+    --file $src \
+    --name "${TARGET}_${package}"
+done
