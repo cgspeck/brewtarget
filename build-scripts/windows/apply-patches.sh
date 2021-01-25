55a5d98c61cb04b73e38a83adbeebcc54178bb73
Chris Speck <cgspeck@gmail.com>
2021-01-26 10:00:23 +1100

    Reference variable and use foward slashes

--- build-scripts/windows/apply-patches.sh
+++ build-scripts/windows/apply-patches.sh
@@ -1,11 +1,11 @@
 #! /bin/bash -ex
 SRC_PATH=$1
-PATCH="${SRC_PATH}/build-scripts/windows/patches/001-omit-missing-libs-for-packager.patch"
+PATCH="${SRC_PATH}\build-scripts\windows\patches\001-omit-missing-libs-for-packager.patch"
 
 echo -e "\nApplying patches\n"
 
 cd $SRC_PATH
-git apply --stat $patch
-git apply --check $patch
-git apply $patch
+git apply --stat $PATCH
+git apply --check $PATCH
+git apply $PATCH
 cd -
